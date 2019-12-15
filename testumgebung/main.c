a7b066e9099ae99d331df0b2b794962f3d970916
cubiee <j.harig95@hotmail.de>
2019-12-15 22:31:36 +0100

    Update main.c

--- testumgebung/main.c
+++ testumgebung/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "i2cLCD.h"
 
-
 int main(void){ 
-  I2CLCD display = i2cLCDInit(0x27,4,16);
+  I2CLCD display;
+  display = i2cLCDInit(0x27,4,16);
   clear(&display);
   display_string(&display, "hallo", 1, 4);
   return 0;
